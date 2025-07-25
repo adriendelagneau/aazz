fe2f251506e40e1c59416f43a09c4c3128c31cc8
b166er <delagneauadrien@yahoo.fr>
2025-07-25 12:23:24 +0200

    first commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,7 @@ export default async function Home() {
 
 
   return (
-    <div className="mx-auto min-h-screen w-full max-w-7xl gap-4 px-4">
+    <div className="mx-auto scrollbar scrollbar-none min-h-screen w-full max-w-7xl gap-4 px-4">
    
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr" suppressHydrationWarning className="scroll-smooth">
+    <html lang="fr" suppressHydrationWarning >
       <body
         className={`${noticaText.variable} ${poppins.variable} scrollbar scrollbar-none`}
       >
