066f9527425c92c7ed701c311ce176d7481d8cfc
b166er <delagneauadrien@yahoo.fr>
2025-07-25 12:18:13 +0200

    first commit

---
+++ types/prisma-plugin.d.ts
@@ -0,0 +1,7 @@
+// types/prisma-plugin.d.ts
+declare module "@prisma/nextjs-monorepo-workaround-plugin" {
+    // You can type the plugin more precisely if needed
+    export class PrismaPlugin {
+        constructor()
+    }
+}
